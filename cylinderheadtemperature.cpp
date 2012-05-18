811a1ae253481ce6333912198a5e206b7d304ab4
Tobias Rad <Tobias Rad@localhost>
2012-05-18 20:08:56 +0200

    Functions in CylinderHeadTemperature added for full functionality

--- cylinderheadtemperature.cpp
+++ cylinderheadtemperature.cpp
@@ -1,18 +1,106 @@
 #include "cylinderheadtemperature.h"
 
 CylinderHeadTemperature::CylinderHeadTemperature(QGraphicsItem *parent) : QGraphicsItem(parent)
+  , minValue(100.0)
+  , maxValue(250.0)
+  , greenYellowValue(200.0)
+  , yellowRedValue(230.0)
 {
+	currentValues << 210.0 << 185.0 << 170.0 << 190.0;
 }
 
 QRectF CylinderHeadTemperature::boundingRect() const
 {
-	return QRectF(-125, -125, 250, 250);
+	return QRectF(-125, -175, 250, 300);
 }
 
 void CylinderHeadTemperature::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-	painter->setBrush(Qt::blue);
-	painter->drawRect(boundingRect());
+	painter->setPen(QPen(Qt::transparent, 0));
 
+	//Draw the side legend
+	painter->setBrush(Qt::green);
+	painter->drawRect(QRectF(QPointF(50.0, calculateLocalValue(minValue)), QPointF(80.0, calculateLocalValue(greenYellowValue))));
+	painter->setBrush(Qt::yellow);
+	painter->drawRect(QRectF(QPointF(50.0, calculateLocalValue(greenYellowValue)), QPointF(80.0, calculateLocalValue(yellowRedValue))));
+	painter->setBrush(Qt::red);
+	painter->drawRect(QRectF(QPointF(50.0, calculateLocalValue(yellowRedValue)), QPointF(80.0, calculateLocalValue(maxValue))));
 
+	//Set painter for texts
+	painter->setPen(QPen(Qt::white, 1));
+	painter->setFont(QFont("Arial", 12));
+
+	//Draw the static texts
+	painter->drawText(QRectF(90.0, -162.5, 35.0, 20.0), Qt::AlignRight | Qt::AlignVCenter, "CHT");
+	painter->drawText(QRectF(90.0, 105.0, 35.0, 20.0), Qt::AlignRight | Qt::AlignVCenter, "°C");
+
+	//Draw the numbers
+	foreach(double value, betweenValues)
+	{
+		painter->drawLine(80, calculateLocalValue(value), 90, calculateLocalValue(value));
+		painter->drawText(QRectF(90.0, calculateLocalValue(value)-10.0, 35.0, 20.0), Qt::AlignRight | Qt::AlignVCenter, QString::number(value, 'f', 0));
+	}
+
+	//Draw the red line
+	painter->setPen(Qt::red);
+	painter->drawLine(-125, calculateLocalValue(yellowRedValue), 35, calculateLocalValue(yellowRedValue));
+
+	//Draw center dashed line
+	QPen middleLinePen(Qt::white, 1, Qt::DashLine);
+	painter->setPen(middleLinePen);
+	for(int i = 0; i < 4; i++)
+	{
+		painter->drawLine(i*40-105, -125, i*40-105, 125);
+	}
+
+	//Draw the bar graphes
+	for(int i = 0; i < 4; i++)
+	{
+		QRectF barRect = QRectF(QPointF(i*40-120, 125), QPointF(i*40-90, calculateLocalValue(currentValues.value(i))));
+		if(currentValues.at(i) > yellowRedValue)
+		{
+			painter->setBrush(Qt::red);
+		}
+		else if(currentValues.at(i) > greenYellowValue)
+		{
+			painter->setBrush(Qt::yellow);
+		}
+		else
+		{
+			painter->setBrush(Qt::green);
+		}
+		painter->setPen(QPen(Qt::transparent, 0));
+		painter->drawRect(barRect);
+		painter->setPen(painter->brush().color());
+		if(painter->brush().color() == Qt::green)
+		{
+			painter->setPen(Qt::white);
+		}
+		QRectF textRect(-30, -20, 60, 40);
+		textRect.moveCenter(QPointF(i*40-105, -140));
+		if(i%2)
+		{
+			textRect.translate(QPointF(0.0, -25.0));
+		}
+		painter->drawText(textRect, Qt::AlignCenter, QString::number(currentValues.at(i), 'f', 0));
+	}
+}
+
+double CylinderHeadTemperature::calculateLocalValue(double value) const
+{
+	return -(value-minValue)/(maxValue-minValue)*250.0+125.0;
+}
+
+void CylinderHeadTemperature::addBetweenValue(double value)
+{
+	betweenValues.append(value);
+}
+
+void CylinderHeadTemperature::setValues(double val1, double val2, double val3, double val4)
+{
+	currentValues.replace(0, val1);
+	currentValues.replace(1, val2);
+	currentValues.replace(2, val3);
+	currentValues.replace(3, val4);
+	update();
 }
--- cylinderheadtemperature.h
+++ cylinderheadtemperature.h
@@ -9,6 +9,14 @@ public:
 	explicit CylinderHeadTemperature(QGraphicsItem * parent = 0);
 	QRectF boundingRect() const;
 	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+	void addBetweenValue(double value);
+	void setValues(double val1, double val2, double val3, double val4);
+private:
+	double calculateLocalValue(double value) const;
+	double minValue, maxValue;
+	double greenYellowValue, yellowRedValue;
+	QList<double> currentValues;
+	QList<double> betweenValues;
 };
 
 #endif // CYLINDERHEADTEMPERATURE_H
