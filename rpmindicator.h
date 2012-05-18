ca1b1afe4495b486c72d6464669457f76556b755
Tobias Rad <t.rad@tu-braunschweig.de>
2012-05-18 17:45:47 +0200

    RpmIndicator introduced

---
+++ rpmindicator.h
@@ -0,0 +1,22 @@
+#ifndef RPMINDICATOR_H
+#define RPMINDICATOR_H
+
+#include <QtGui>
+
+class RpmIndicator : public QGraphicsItem
+{
+public:
+	RpmIndicator(QGraphicsItem * parent = 0);
+	~RpmIndicator();
+	QRectF boundingRect() const;
+	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+	void addBetweenValue(double value);
+	void setValue(double value);
+private:
+	double calculateLocalValue(double value) const;
+	double minValue, maxValue, currentValue;
+	double whiteGreenBorder, greenRedBorder;
+	QList<double> beetweenValues;
+};
+
+#endif // RPMINDICATOR_H
