ec11f1edb1550249a84391dc46316de918a2573d
Tobias Rad <Tobias Rad@localhost>
2012-05-18 13:28:56 +0200

    BarGraph implemented in the EngineMonitor

--- enginemonitor.cpp
+++ enginemonitor.cpp
@@ -1,10 +1,82 @@
 #include "enginemonitor.h"
 
 EngineMonitor::EngineMonitor(QWidget *parent) : QGraphicsView(parent)
+  , graphicsScene(this)
 {
-	this->setWindowFlags(Qt::FramelessWindowHint);
+	setWindowFlags(Qt::FramelessWindowHint);
+
+	graphicsScene.setBackgroundBrush(Qt::black);
+	setScene(&graphicsScene);
+
+
+	setupBarGraphs();
+
+	oilTemperature.setValue(149.0);
+	oilPressure.setValue(49.0);
+	voltMeter.setValue(14.0);
+	ampereMeter.setValue(0.0);
+	fuelFlow.setValue(14.9);
+	outsideAirTemperature.setValue(24.0);
 }
 
 EngineMonitor::~EngineMonitor()
 {
+}
+
+void EngineMonitor::setupBarGraphs()
+{
+	oilTemperature.setPos(0, 0);
+	oilTemperature.setTitle("OIL T");
+	oilTemperature.setUnit("°C");
+	oilTemperature.setBorders(80.0, 180.0);
+	oilTemperature.addColorStop(ColorStop(Qt::green, 80.0, 160.0));
+	oilTemperature.addColorStop(ColorStop(Qt::yellow, 160.0, 180.0));
+	graphicsScene.addItem(&oilTemperature);
+
+	oilPressure.setPos(100, 0);
+	oilPressure.setTitle("OIL P");
+	oilPressure.setUnit("psi");
+	oilPressure.setBorders(0.0, 60.0);
+	oilPressure.addColorStop(ColorStop(Qt::red, 0.0, 15.0));
+	oilPressure.addColorStop(ColorStop(Qt::yellow, 55.0, 60.0));
+	oilPressure.addBetweenValue(15.0);
+	oilPressure.addBetweenValue(35.0);
+	oilPressure.addBetweenValue(50.0);
+	graphicsScene.addItem(&oilPressure);
+
+	voltMeter.setPos(0, 150);
+	voltMeter.setTitle("VOLTS");
+	voltMeter.setUnit("V");
+	voltMeter.setBorders(11.5, 18.0);
+	voltMeter.addColorStop(ColorStop(Qt::yellow, 11.5, 13.0));
+	voltMeter.setPrecision(1, 1);
+	graphicsScene.addItem(&voltMeter);
+
+	ampereMeter.setPos(100, 150);
+	ampereMeter.setTitle("AMPS");
+	ampereMeter.setUnit("A");
+	ampereMeter.setBorders(-50.0, 50.0);
+	ampereMeter.addColorStop(ColorStop(Qt::red, -50.0, 0.0));
+	ampereMeter.addColorStop(ColorStop(Qt::yellow, 30.0, 50.0));
+	ampereMeter.addBetweenValue(0.0);
+	graphicsScene.addItem(&ampereMeter);
+
+	fuelFlow.setPos(0, 300);
+	fuelFlow.setTitle("FF");
+	fuelFlow.setUnit("lph");
+	fuelFlow.setBorders(0, 30);
+	fuelFlow.addBetweenValue(10.0);
+	fuelFlow.setPrecision(1);
+	fuelFlow.addBetweenValue(20.0);
+	graphicsScene.addItem(&fuelFlow);
+
+	outsideAirTemperature.setPos(100, 300);
+	outsideAirTemperature.setTitle("OAT");
+	outsideAirTemperature.setUnit("°C");
+	outsideAirTemperature.setBorders(-10.0, 40);
+	outsideAirTemperature.addBetweenValue(0.0);
+	outsideAirTemperature.addBetweenValue(10.0);
+	outsideAirTemperature.addBetweenValue(20.0);
+	outsideAirTemperature.addBetweenValue(30.0);
+	graphicsScene.addItem(&outsideAirTemperature);
 }
--- enginemonitor.h
+++ enginemonitor.h
@@ -3,12 +3,23 @@
 
 #include <QtGui>
 
+#include "bargraph.h"
+
 class EngineMonitor : public QGraphicsView
 {
 	Q_OBJECT
 public:
 	EngineMonitor(QWidget *parent = 0);
 	~EngineMonitor();
+private:
+	void setupBarGraphs();
+	QGraphicsScene graphicsScene;
+	BarGraph oilTemperature;
+	BarGraph oilPressure;
+	BarGraph voltMeter;
+	BarGraph ampereMeter;
+	BarGraph fuelFlow;
+	BarGraph outsideAirTemperature;
 };
 
 #endif // ENGINEMONITOR_H
