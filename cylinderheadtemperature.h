53c8a05717a1ff73ebf4e32e59d2d2593566c852
Tobias Rad <t.rad@tu-braunschweig.de>
2012-05-18 22:14:47 +0200

    setBorders in CylinderHeadTemperature included

--- cylinderheadtemperature.h
+++ cylinderheadtemperature.h
@@ -9,6 +9,7 @@ public:
 	explicit CylinderHeadTemperature(QGraphicsItem * parent = 0);
 	QRectF boundingRect() const;
 	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+	void setBorders(double minimum, double maximum, double yellowBorder, double redBorder);
 	void addBetweenValue(double value);
 	void setValues(double val1, double val2, double val3, double val4);
 private:
