2b0836074632005a475d01eb14b27a6d423c88dc
Tobias Rad <t.rad@tu-braunschweig.de>
2012-05-19 16:09:37 +0200

    GPLv3 added in source files and as single text files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,51 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// EngineMonitor, a graphical gauge to monitor an aircraft's engine     //
+// Copyright (C) 2012 Tobias Rad                                        //
+//                                                                      //
+// This program is free software: you can redistribute it and/or modify //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation, either version 3 of the License, or    //
+// (at your option) any later version.                                  //
+//                                                                      //
+// This program is distributed in the hope that it will be useful,      //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of       //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        //
+// GNU General Public License for more details.                         //
+//                                                                      //
+// You should have received a copy of the GNU General Public License    //
+// along with this program. If not, see <http://www.gnu.org/licenses/>. //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
 #include <QtGui/QApplication>
 #include "enginemonitor.h"
+
+class I : public QThread
+{
+public:
+	static void sleep(unsigned long secs)
+	{
+		QThread::sleep(secs);
+	}
+};
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	QPixmap pixmap("splashscreen_1024.png");
+	QSplashScreen splash(pixmap);
+	splash.show();
+//	a.processEvents();
+
 	EngineMonitor engineMonitor;
+	I::sleep(5);
 	engineMonitor.show();
 	engineMonitor.move(0, 0);
 	engineMonitor.resize(800, 600);
+	splash.finish(&engineMonitor);
+//	engineMonitor.showFullScreen();
+//	engineMonitor.scale(1.28, 1.28);
 	return a.exec();
 }
