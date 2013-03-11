a424035811312986173079a31b9e2a7b67e83105
Tobias Rad <t.rad@tu-braunschweig.de>
2013-03-11 22:54:12 +0100

    Reading TC5-8 as CHT temperature

--- enginemonitor.cpp
+++ enginemonitor.cpp
@@ -179,6 +179,11 @@ void EngineMonitor::setDataMessage4egt(quint16 egt1, quint16 egt2, quint16 egt3,
 	exhaustGasTemperature.setValues(egt1, egt2, egt3, egt4);
 }
 
+void EngineMonitor::setDataMessage4cht(quint16 cht1, quint16 cht2, quint16 cht3, quint16 cht4)
+{
+	cylinderHeadTemperature.setValues(cht1, cht2, cht3, cht4);
+}
+
 void EngineMonitor::userMessageHandler(QString title, QString content, bool endApplication)
 {
 	QMessageBox::warning(this, title, content);
