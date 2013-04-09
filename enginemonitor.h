b50655f6575769d8d2de164934d12bf43218298f
Tobias Rad <Tobias Rad@localhost>
2013-04-09 23:27:49 +0200

    Inside air temperature gets promoted to display

--- enginemonitor.h
+++ enginemonitor.h
@@ -69,7 +69,7 @@ private slots:
 	void writeLogFile();
 public slots:
 	void setDataMessage1(double fuelFlowValue);
-	void setDataMessage2(double outsideAirTemperatureValue, double ampereValue, double oilTemperatureValue, double oilPressureValue, double voltageValue, double manifoldPressureValue);
+	void setDataMessage2(double insideAirTemperatureValue, double outsideAirTemperatureValue, double ampereValue, double oilTemperatureValue, double oilPressureValue, double voltageValue, double manifoldPressureValue);
 	void setDataMessage3(double revolutionPerMinute);
 	void setDataMessage4egt(quint16 egt1, quint16 egt2, quint16 egt3, quint16 egt4);
 	void setDataMessage4cht(quint16 cht1, quint16 cht2, quint16 cht3, quint16 cht4);
