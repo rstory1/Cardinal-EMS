15758fd0b709ac6f14c41a3e9fe7cdad8ec8428a
Ryan Story <rstory1@gmail.com>
2017-03-16 15:56:43 -0500

    Added class to show a button bar for some soft keys

---
+++ buttonbar.cpp
@@ -0,0 +1,6 @@
+#include "buttonbar.h"
+
+buttonBar::buttonBar()
+{
+
+}
---
+++ buttonbar.h
@@ -0,0 +1,11 @@
+#ifndef BUTTONBAR_H
+#define BUTTONBAR_H
+
+
+class buttonBar : public QGraphicObject
+{
+public:
+    buttonBar();
+};
+
+#endif // BUTTONBAR_H
